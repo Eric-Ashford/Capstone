d42f8ebf58d2c43443f0408656b5969007335d02
Eric Ashford <eri.ashford@gmail.com>
2018-09-14 11:05:33 -0500

    initial commit
    adding all the files

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class cameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        transform.position = player.transform.position + offset;
+
+
+        //Get rid of this crap as soon as possible, worst camera I ever made LOLOLOLOLOLLOLOLLOLOLOLOLOLOLOLOL
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(-Vector3.up * 100 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(Vector3.up * 100 * Time.deltaTime);
+        }
+
+    }
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    private Rigidbody _rb;
+
+    //[SerializeField]
+    public float walkingSpeed = 2f;
+
+    //[SerializeField]
+    public float currentSpeed;
+    
+    //[SerializeField]
+    public float sprintSpeed = 900f;
+
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+       _rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+     
+        float moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * 8f;
+ 
+
+        float moveVertical = Input.GetAxis("Vertical") * Time.deltaTime * 8f;
+
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+
+
+        if ((Input.GetKeyDown(KeyCode.LeftShift) == true))
+        {
+            currentSpeed = sprintSpeed;
+        }
+        else
+        {
+            currentSpeed = walkingSpeed;
+        }
+
+        _rb.AddForce(movement * currentSpeed);
+
+    }
+}
